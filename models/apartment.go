21b743603bea7b84cc6937bb012d4fe3c52618bb
Alexander <sav3nme@gmail.com>
2020-02-10 12:39:44 +0300

    apartment: add house and apartment complex to struct

--- models/apartment.go
+++ models/apartment.go
@@ -1,8 +1,10 @@
 package models
 
 type Apartment struct {
-	Floor int
-	Rooms int
-	Area  float64
-	Rent  float64
+	Floor            int
+	Rooms            int
+	Area             float64
+	Rent             float64
+	House            *House
+	ApartmentComplex *ApartmentComplex
 }
